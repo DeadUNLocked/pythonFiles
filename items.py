8226e6118434a9de9b7544f9836e65a761e7ddd8
ClubStep <qqkingcary@gmail.com>
2025-07-07 21:37:20 +0000

    Pending changes exported from your codespace

---
+++ items.py
@@ -0,0 +1,16 @@
+METAL = [
+    "pencil lead",
+    "paper clips",
+    "push pins",
+    "staples",
+    "nails",
+    "gum wrappers",
+    "scissors,"
+]
+
+def main():
+    for item in ITEMS:
+        print(item)
+
+
+main()
---
+++ message.py
@@ -0,0 +1,5 @@
+# ask for username
+name = input("who are you")
+
+# troll the user
+print("get a life, ", name, sep="crappy ")
